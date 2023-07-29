bc950f007088322e3a23a53a110c4472eed56e47
Giulio Benedetti <giuliobene2000@gmail.com>
2023-07-29 11:22:32 +0900

    Complete OKeefe example

--- OKeefeDSData/import.R
+++ OKeefeDSData/import.R
@@ -8,14 +8,22 @@ url_to_assay <- url("https://github.com/microbiome/data/raw/main/OKeefeDSData/ok
 url_to_coldata <- url("https://github.com/microbiome/data/raw/main/OKeefeDSData/okeefe_coldata.csv")
 url_to_rowdata <- url("https://github.com/microbiome/data/raw/main/OKeefeDSData/okeefe_rowdata.csv")
 
+
 ### Importing FILES ###
 
-okeefe_assay <- read.csv(url_to_assay)
-okeefe_coldata <- read.csv(url_to_coldata)
-okeefe_rowdata <- read.csv(url_to_rowdata)
+okeefe_assay <- read.csv(url_to_assay, row.names = 1)
+colnames(okeefe_assay) <- gsub("\\.", "-", colnames(okeefe_assay))
+
+okeefe_coldata <- read.csv(url_to_coldata, row.names = 1)
+okeefe_rowdata <- read.csv(url_to_rowdata, row.names = 1)
+
 
 ### Constructing TSE ###
 
 tse <- TreeSummarizedExperiment(assays = list(counts = okeefe_assay),
                                 colData = okeefe_coldata,
                                 rowData = okeefe_rowdata)
+
+
+### SAVE into RDS ###
+saveRDS(tse, file = "tse.rds")

513035bd3faa582d3208b62653aba647df5fa265
Giulio Benedetti <giuliobene2000@gmail.com>
2023-07-29 10:51:07 +0900

    Complete Hintikka example

--- HintikkaXOData/import.R
+++ HintikkaXOData/import.R
@@ -15,12 +15,12 @@ url_to_metabolites_rowdata <- url("https://github.com/microbiome/data/raw/main/H
 url_to_biomarkers_rowdata <- url("https://github.com/microbiome/data/raw/main/HintikkaXOData/hintikka_biomarkers_rowdata.csv")
 
 ## samples
-url_to_sample_data <- url("https://github.com/microbiome/data/raw/hintikka/HintikkaXOData/hintikka_sample_data.csv")
-#url_to_sample_map
+url_to_sample_data <- url("https://github.com/microbiome/data/raw/main/HintikkaXOData/hintikka_sample_data.csv")
+url_to_sample_map <- url("https://github.com/microbiome/data/raw/main/HintikkaXOData/hintikka_sample_map.csv")
 
 ### Constructing TSEs ###
 
-## microbiome TSE
+## microbiome experiment
 
 microbiome_assay <- read.csv(url_to_microbiome_assay, row.names = 1)
 microbiome_rowdata <- read.csv(url_to_microbiome_rowdata, row.names = 1)
@@ -28,7 +28,7 @@ microbiome_rowdata <- read.csv(url_to_microbiome_rowdata, row.names = 1)
 microbiome_tse <- TreeSummarizedExperiment(assays = list(counts = microbiome_assay),
                                            rowData = microbiome_rowdata)
 
-## metabolites TSE
+## metabolites experiment
 
 metabolites_assay <- read.csv(url_to_metabolites_assay, row.names = 1)
 metabolites_rowdata <- read.csv(url_to_metabolites_rowdata, row.names = 1, header = FALSE)
@@ -37,7 +37,7 @@ metabolites_rowdata$V2 <- NULL
 metabolites_tse <- TreeSummarizedExperiment(assays = list(nmr = metabolites_assay),
                                             rowData = metabolites_rowdata)
 
-## biomarkers TSE
+## biomarkers experiment
 
 biomarkers_assay <- read.csv(url_to_biomarkers_assay, row.names = 1)
 biomarkers_rowdata <- read.csv(url_to_biomarkers_rowdata, row.names = 1, header = FALSE)
@@ -47,9 +47,17 @@ biomarkers_tse <- TreeSummarizedExperiment(assays = list(signals = biomarkers_as
                                            rowData = biomarkers_rowdata)
 
 
+### Constructing MAE ###
+
 sample_data <- read.csv(url_to_sample_data, row.names = 1)
+sample_map <- read.csv(url_to_sample_map)
 
 mae <- MultiAssayExperiment(experiments = ExperimentList(microbiota = microbiome_tse,
                                                          metabolites = metabolites_tse,
                                                          biomarkers = biomarkers_tse),
-                            colData = sample_data)
+                            colData = sample_data,
+                            sampleMap = sample_map)
+
+
+### SAVE into RDS ###
+saveRDS(mae, file = "mae.rds")
